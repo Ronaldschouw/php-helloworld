94cf07d901f3abd1f087d54dd726bcfa44e9c86c
root <root@ronalt.nl>
2020-12-08 21:17:52 +0100

    update 2020

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";<br />
-?>
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+?><br />
 
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br />
