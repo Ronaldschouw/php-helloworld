d0c34585976ad885053aa6f895d32f63ea073fe8
root <root@ronalt.nl>
2020-12-08 21:56:17 +0100

    update 2020

--- index.php
+++ index.php
@@ -6,11 +6,8 @@
 
 <body>
 
-<?php echo "Hello World!"; ?><br /><br /><br /><br />
-
 <?php print "Hello, World! php version is " . PHP_VERSION . "\n";
 ?>
-<?php if($_ENV["HOSTNAME"]) {?>My hostname is <?php echo $_ENV["HOSTNAME"]; ?>}<br /><br />
 
 <?php echo "r.schouw@fullstaq.com"; ?>
 </body>
